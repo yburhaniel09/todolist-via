55912564c37b7416791423cd35c94eaf8e63d698
Ybur Haniel <ayahaniel09@gmail.com>
2019-10-04 23:18:31 +0400

    vue fix

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,7 +7,6 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,7 +14,11 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+import Vue from 'vue'
 Vue.component('tasklist', require('./components/Tasklist.vue'));
+
+
+window.Vue = require('vue');
 
 const app = new Vue({
     el: '#app'
