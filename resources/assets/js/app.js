1e279b7fc7d5c3e8c5c2c4a2d5868c3b8719daf2
Ybur Haniel <ayahaniel09@gmail.com>
2019-10-04 22:33:34 +0400

    bug fix vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -14,9 +14,6 @@ window.Vue = require('vue');
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
-import tasklist from './components/Tasklist.vue';
-
 Vue.component('tasklist', require('./components/Tasklist.vue').default);
 
 const app = new Vue({
